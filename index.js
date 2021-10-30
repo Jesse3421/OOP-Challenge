0c9b21c862c895910bfebd399b4db9c4077434e1
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2021-10-30 13:17:36 -0500

    working on generatepage

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Intern = require("./lib/Intern")
 const Engineer = require("./lib/Engineer")
 const Employee = require("./lib/Employee")
 const { choices } = require("yargs")
+const generatePage = require("./template.js")
 const employeeArr = []
 //const generatePage = require("./src/template.html")
 //import classes (manager, intern, engineer) 
@@ -35,8 +36,8 @@ const managerPrompt = () => {
         },
     ])
 }
-const promptPositions = answers => {
-    console.log(answers)
+//prompt the user to either chose between engineer/intern or pick third option which should generate a manager
+const promptPositions = () => {
     return inquirer.prompt([
         {
             type: "list",
@@ -45,97 +46,100 @@ const promptPositions = answers => {
             choices: ["engineer", "intern", "OR finish building your team"] 
         }
     ])
-    .then(answers => {
-        if(answers.choices == "engineer") {
-            return promptEngineer(answers)
-        } else if(answers.choices == "intern"){
-            return promptIntern(answers)
-        } else {
-        return answers  
-        }
-    })
 }
 
-const promptEngineer = (answers) => {
+const promptEngineer = () => {
     return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your engineers's id number?"
+        },
+        {   type: "input",
+            name: "email",
+            message: "What is your engineers's email address?"
+        },
         {
             type: "input",
             name: "github",
             message: "Please enter your github username"
         }
     ])
-}
+    .then(({name, id, email, github}) => {
+        const engineer = new Engineer(name, id, email, github)
+        employeeArr.push(engineer)
+        console.log(employeeArr)
+        promptPositions()
+    })
+    }
 
-const promptIntern = (answers) => {
+
+const promptIntern = () => {
     return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your interns's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your interns's id number?"
+        },
+        {   type: "input",
+            name: "email",
+            message: "What is your interns's email address?"
+        },
         {
             type:"input",
             name: "school",
             message: "What school is the intern attending?"
         }
     ])
+    .then(({name, id, email, school}) => {
+        const intern = new Intern(name, id, email, school)
+        employeeArr.push(intern)
+        console.log(employeeArr)
+        promptPositions()
+    })
 }
 
-managerPrompt()    
+managerPrompt()
+    .then(({name, id, email, officeNumber}) => {
+        const manager = new Manager(name, id, email, officeNumber)
+        employeeArr.push(manager)
+        console.log(employeeArr)
+    })    
     .then(promptPositions) 
-    .then (answers => {
+    .then(answers => {
+        
         console.log(answers)
+            if(answers.positions === "engineer") {
+                return promptEngineer()
+            } else if(answers.positions == "intern"){
+                return promptIntern()
+            } else {
+            return 
+            }
+    })
+    .then(generatePage = employeeArr => {
+        
+        const pageHTML = generatePage(employeeArr)
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err)
+            console.log("Check out your new list of employees in the index.html file in the directory")
+        })
         
     })
 
-    //.then(({name, id, email, officeNumber}) => {
-      //  this.manager = new Manager(name, id, email, officeNumber)
-        //console.log(this.manager)
-        //employeeArr.push(this.manager)
-        //console.log(employeeArr)
-      //  .then(({name, id, email, officeNumber}) => {
-       //     this.employee = new Employee(name, id, email, officeNumber)
-        //    console.log(this.employee)
-        //})
-
-
+    
+    
+        
     
 
-        //const promptData = promptPositions(answers).positions 
-        //console.log(promptData)
     
-    //      switch (promptData){
-    //      case "engineer":
-    //          return Engineer
-    //          break
-    //     case "intern":
-    //         return Intern
-    //         break
-    //     case "OR finish building your team":
-    //         return Manager
-    //         break
-    //         default: 
-    //         console.log("Please choose one of the options")
-    //         promptPositions()
-    //      }
-    //     console.log(promptData)
-    // })
-    
-
-   // const manager = new Manager(response.mangerName, response. managerID...)
-// employees.push(manager)
-// call switch case function to direct traffic
-
-/*const position = answers.positions
-        console.log(position)
-            switch (position){
-                case "engineer":
-                    console.log("engineer was chosen")
-                    promptEngineer(answers)
-                    this.position = new Engineer(position)
-                    console.log(this.position)
-                break
-                case "intern":
-                    console.log("intern was chosen")
-                break
-                case "OR finish building your team":
-                    return answers
-                    
-                default:
-                    console.log("this didn't work")
-            } */
---
+++ template.js
@@ -0,0 +1,61 @@
+const employeeArr = require('./index')
+
+
+function buildTeam(employeeArr){
+    for(i = 0; i > employeeArr.length; i++) {
+        let team = ''
+        let role = employeeArr[i].getRole()
+        console.log(role)
+        let additionalData = ''
+
+        if(role === 'Manager'){
+            additionalData = employeeArr[i].officeNumber
+        } else if(role === 'Intern'){
+            additionalData = employeeArr[i].school
+        } else if(role === 'Engineer') {
+            additionalData = employeeArr[i].github
+        } else {
+            console.log('The role does not exist')
+        }
+        team = team + `<div class="card" style="width: 18rem;">
+                       <div class="card-header">
+                       ${role}
+                       </div>
+                            <ul class="list group list-group-flush">
+                            <li class="list-group-item"> ${employeeArr[i].name} </li>
+                            <li class="list-group-item"> ${employeeArr[i].id} </li> 
+                            <li class="list-group-item"> ${employeeArr[i].email} </li>
+                            <li class="list-group-item"> ${additionalData} </li>
+                            </ul>
+                        </div>
+                        </div>`
+    }
+}
+
+
+function generatePage(employeeArr) {
+console.log(employeeArr)
+buildTeam(employeeArr)
+
+return`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Team</title>
+</head>
+<body>
+    <div class="" id="">
+    ${team}
+    </div>
+</body>
+<script>
+</html>
+`
+}
+
+
+module.exports = generatePage;
